1669c420af6cc44bd0f8976e8d34d9a573d3a943
Cheng <lichchchn@gmail.com>
2023-09-10 13:05:27 +0800

    update alias

--- init.zsh
+++ init.zsh
@@ -5,4 +5,47 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
+typeset -g HISTFILE=${ZDOTDIR:-${HOME}}/.zsh_history
+
+if (( $+commands[zoxide] )); then
+  export _ZO_ECHO=1
+  eval "$(zoxide init zsh)"
+  alias zz=__zoxide_zi
+fi
+
+
+if (( $+commands[lazygit] )); then
+  alias lg=lazygit
+fi
+
+if (( $+commands[exa] )); then
+  export EXA_COLORS='da=1;34:gm=1;34'
+
+  alias ls='exa --group-directories-first'
+  alias ll='ls -l --git'        # Long format, git status
+  alias l='ll -a'               # Long format, all files
+  alias la='ll -a'              # Long format, all files
+  alias lr='ll -T'              # Long format, recursive as a tree
+  alias lx='ll -sextension'     # Long format, sort by extension
+  alias lk='ll -ssize'          # Long format, largest file size last
+  alias lt='ll -smodified'      # Long format, newest modification time last
+  alias lc='ll -schanged'       # Long format, newest status change (ctime) last
+fi
+
+if (( $+commands[nvim] )); then
+  alias vim=nvim
+fi
+
+if (( $+commands[tmux] )); then
+  alias t="tmux new -As0"
+  alias tm="tmux"
+fi
+
+
+alias vz="vim ~/.zshrc && source ~/.zshrc"
+alias vzz="vim ~/.zimrc"
+alias nv="nvim"
+alias gfc="git clone --recursive"
+alias gst="git status"
+
 
